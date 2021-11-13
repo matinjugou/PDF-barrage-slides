8960997d4b86d07ab01feaaf6dec1c29f896fdb9
Huang Chao <thss15_huangc@163.com>
2021-11-13 16:07:00 +0800

    Reset clean

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,7 @@
       :speeds="170"
       :random-channel="true"
       extra-style="color: black"
-      style="z-index: 2;position:absolute;width: 100%;height:50%;top: 50%;
+      style="z-index: 2;position:absolute;width: 100%;height:80%;top: 50%;
     left:50%;
     transform: translate(-50% , -50%);"
     />
@@ -118,7 +118,7 @@ export default {
           type: 'success',
           message: '弹幕已清空',
         });
-        this.arr = [];
+        window.location.reload();
       });
     },
     switchPause() {
