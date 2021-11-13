bd97b959a5a26cbc4168bc75087f2ac5d854f188
Huang Chao <thss15_huangc@163.com>
2021-11-13 13:10:14 +0800

    Add Backend Script

---
+++ backend/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+barrage_pools = {}
+
+@app.route("/")
+def helloWorld():
+  return "Hello!"
+
+@app.route("/send", methods=['POST'])
+def send_barrage():
+    channel = request.form.get('channel', '')
+    message = request.form.get('message', '')
+    if barrage_pools.get(channel, None) is None:
+        barrage_pools[channel] = []
+    barrage_pools[channel].append(message)
+    return make_response(jsonify({'message': f"Add message to channel: {channel}"}))
+    
+
+@app.route("/clean", methods=['POST'])
+def clean_barrage():
+    channel = request.form.get('channel', '')
+    if barrage_pools.get(channel, None) is not None:
+        del barrage_pools[channel]
+    return make_response(jsonify({'message': f'Clean out messages in channel: {channel}'}))
+
+@app.route("/get", methods=['GET'])
+def get_barrage():
+    channel = request.args.get('channel', '')
+    index = int(request.args.get('index', 0))
+    res = barrage_pools.get(channel, [])
+    return make_response(jsonify(res[index:]))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
